1d37ba9809fda248fcf0e4a7897fd9e3ec3598b5
ChaosA156 <ChaosA156@gmail.com>
2024-02-27 15:16:36 -0600

    More Menu Process
    Made progress on the main menu.

---
+++ Assets/Scripts/Shield&Health/HealthManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public Image healthBar;
+    public float healthAmount = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            TakeDamage(20);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Heal(20);
+        }
+
+        if (healthAmount <= 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+        if (healthAmount > 100)
+        {
+            healthAmount = 100;
+            healthBar.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (healthAmount < 0)
+        {
+            healthAmount = 0;
+            healthBar.transform.localScale = new Vector3(0, 1, 1);
+        }
+    }
+
+   
+
+    public void TakeDamage(float damage)
+    {
+        healthAmount -= damage;
+        healthBar.transform.localScale = new Vector3(healthAmount/100, 1, 1);
+        //healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    public void Heal(float healingAmount)
+    {
+        healthAmount += healingAmount;
+        healingAmount = Mathf.Clamp(healthAmount, 0, 100);
+
+        healthBar.transform.localScale = new Vector3(healthAmount/100, 1, 1);
+        //healthBar.fillAmount = healthAmount / 100f;
+    }
+}
+// Heal to recover health, note for later code.
---
+++ Assets/Scripts/Shield&Health/ShieldManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShieldManager : MonoBehaviour
+{
+    public Image shieldBar;
+    public float shieldAmount = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Return))
+        {
+            TakeDamage(20);
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Recharge(20);
+        }
+
+        if (shieldAmount > 100)
+        {
+            shieldAmount = 100;
+            shieldBar.transform.localScale = new Vector3(1, 1, 1);
+        }
+      
+        if (shieldAmount < 0)
+        {
+            shieldAmount = 0;
+            shieldBar.transform.localScale = new Vector3(0, 1, 1);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        shieldAmount -= damage;
+        shieldBar.transform.localScale = new Vector3(shieldAmount / 100, 1, 1);
+        //shieldBar.fillAmount = shieldAmount / 100f;
+    }
+
+    public void Recharge(float healingAmount)
+    {
+        shieldAmount += healingAmount;
+        healingAmount = Mathf.Clamp(shieldAmount, 0, 100);
+        shieldBar.transform.localScale = new Vector3(shieldAmount / 100, 1, 1);
+        //shieldBar.fillAmount = shieldAmount / 100f;
+    }
+}
+// Recharge to recover shields, note for later code.
